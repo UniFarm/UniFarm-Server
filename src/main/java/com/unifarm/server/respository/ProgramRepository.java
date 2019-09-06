415d8b82874aff33360f760a755f916114d33786
seong4862 <nuli95@naver.com>
2019-09-07 05:31:50 +0900

    실시간 인기 프로그램

--- src/main/java/com/unifarm/server/respository/ProgramRepository.java
+++ src/main/java/com/unifarm/server/respository/ProgramRepository.java
@@ -1,7 +1,11 @@
 package com.unifarm.server.respository;
 
 import com.unifarm.server.domain.Program;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -9,4 +13,9 @@ import java.util.Optional;
 public interface ProgramRepository extends JpaRepository<Program, Integer> {
 
     Optional<Program> findByProgramIdx(int programIdx);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Program program set program.viewCount = program.viewCount + 1 where program.programIdx = :programIdx")
+    void updateViewCount(@Param("programIdx") int programIdx);
 }
